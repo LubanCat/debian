644c5961c6f65437e5e158e5a75780b7bb29a9d0
hejiawencc <hjw0415@outlook.com>
2023-04-07 15:20:40 +0800

    rootfs 完善构建脚本

--- mk-buster-desktop.sh
+++ mk-buster-desktop.sh
@@ -23,11 +23,13 @@ install_packages() {
 		MALI=bifrost-g52-g2p0
 		ISP=rkaiq_rk3568
 		RGA=rga
+		MIRROR=carp-rk356x
 		;;
         rk3588|rk3588s)
 		ISP=rkaiq_rk3588
 		MALI=valhall-g610-g6p0
 		RGA=rga2
+		# MIRROR=carp-rk3588
 		;;
     esac
 }
@@ -61,6 +63,7 @@ trap finish ERR
 sudo rm -rf binary/
 
 echo -e "\033[36m Extract image \033[0m"
+sudo rm -rf $TARGET_ROOTFS_DIR
 sudo tar -xpf linaro-buster-desktop-alip-*.tar.gz
 
 # packages folder
@@ -103,10 +106,10 @@ sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 
-echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian buster main" | sudo tee -a /etc/apt/sources.list
-echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian carp-rk356x main" | sudo tee -a /etc/apt/sources.list
-
-curl https://Embedfire.github.io/keyfile | sudo apt-key add -
+if [ $MIRROR ]; then
+	echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian $MIRROR main" | sudo tee -a /etc/apt/sources.list
+	curl https://Embedfire.github.io/keyfile | sudo apt-key add -
+fi
 
 apt-get update
 apt-get upgrade -y
@@ -116,7 +119,7 @@ chmod +x /etc/rc.local
 
 export APT_INSTALL="apt-get install -fy --allow-downgrades"
 
-#--------------- lubancat --------------
+echo -e "\033[47;36m ---------- LubanCat -------- \033[0m"
 \${APT_INSTALL} toilet mpv
 
 passwd root <<IEOF
@@ -141,15 +144,13 @@ chown -hR cat:cat /home/cat/.config
 \${APT_INSTALL} numix-gtk-theme numix-icon-theme-circle
 ln -sf /etc/alternatives/lubancat-wallpaper.png /etc/alternatives/desktop-background
 
-#---------------power management --------------
+apt install -fy --allow-downgrades /packages/install_packages/*.deb
+
+echo -e "\033[47;36m ----- power management ----- \033[0m"
 \${APT_INSTALL} pm-utils triggerhappy
 cp /etc/Powermanager/triggerhappy.service  /lib/systemd/system/triggerhappy.service
 
-#---------------Rga--------------
-\${APT_INSTALL} /packages/rga/*.deb
-
-echo -e "\033[36m Setup Video.................... \033[0m"
-
+echo -e "\033[47;36m ------ Setup Video---------- \033[0m"
 \${APT_INSTALL} /packages/mpp/*
 \${APT_INSTALL} /packages/gst-rkmpp/*.deb
 \${APT_INSTALL} /packages/gstreamer/*.deb
@@ -158,63 +159,51 @@ echo -e "\033[36m Setup Video.................... \033[0m"
 \${APT_INSTALL} /packages/gst-plugins-good1.0/*.deb
 \${APT_INSTALL} /packages/gst-plugins-ugly1.0/*.deb
 
-#---------Camera---------
-echo -e "\033[36m Install camera.................... \033[0m"
+echo -e "\033[47;36m ----- Install Camera ----- - \033[0m"
 \${APT_INSTALL} cheese v4l-utils
 \${APT_INSTALL} /packages/libv4l/*.deb
 
-#---------Xserver---------
-echo -e "\033[36m Install Xserver.................... \033[0m"
+echo -e "\033[47;36m ----- Install Xserver------- \033[0m"
 \${APT_INSTALL} /packages/xserver/*.deb
 
-#---------------Openbox--------------
-echo -e "\033[36m Install openbox.................... \033[0m"
+echo -e "\033[47;36m ----- Install openbox------- \033[0m"
 \${APT_INSTALL} /packages/openbox/*.deb
 
-#---------update chromium-----
+echo -e "\033[47;36m ------ update chromium ----- \033[0m"
 \${APT_INSTALL} /packages/chromium/*.deb
 
-#------------------libdrm------------
-echo -e "\033[36m Install libdrm.................... \033[0m"
+echo -e "\033[47;36m ------- Install libdrm ------ \033[0m"
 \${APT_INSTALL} /packages/libdrm/*.deb
 
-#------------------libdrm-cursor------------
-echo -e "\033[36m Install libdrm-cursor.................... \033[0m"
+echo -e "\033[47;36m ------ libdrm-cursor -------- \033[0m"
 \${APT_INSTALL} /packages/libdrm-cursor/*.deb
 
 # Only preload libdrm-cursor for X
 sed -i "/libdrm-cursor.so/d" /etc/ld.so.preload
 sed -i "1aexport LD_PRELOAD=libdrm-cursor.so.1" /usr/bin/X
 
-#------------------pcmanfm------------
-#echo -e "\033[36m Install pcmanfm.................... \033[0m"
+echo -e "\033[47;36m ----- Install pcmanfm ------- \033[0m"
 #\${APT_INSTALL} /packages/pcmanfm/*.deb
 
-#------------------blueman------------
-echo -e "\033[36m Install blueman.................... \033[0m"
+echo -e "\033[47;36m ------ Install blueman ------ \033[0m"
 \${APT_INSTALL} blueman
 echo exit 101 > /usr/sbin/policy-rc.d
 chmod +x /usr/sbin/policy-rc.d
 \${APT_INSTALL} blueman
 rm -f /usr/sbin/policy-rc.d
 
-#------------------rkwifibt------------
-echo -e "\033[36m Install rkwifibt.................... \033[0m"
-\${APT_INSTALL} /packages/rkwifibt/*.deb
-ln -sf /system/etc/firmware /vendor/etc/
-
 if [ "$VERSION" == "debug" ]; then
-#------------------glmark2------------
-echo -e "\033[36m Install glmark2.................... \033[0m"
+echo -e "\033[47;36m ------ Install glmark2 ------ \033[0m"
 \${APT_INSTALL} /packages/glmark2/*.deb
 fi
 
-#------------------rknpu2------------
-echo -e "\033[36m Install rknpu2.................... \033[0m"
-tar xvf /packages/rknpu2/*.tar -C /
+if [ -e "/usr/lib/aarch64-linux-gnu" ] ;
+then
+echo -e "\033[47;36m ------- move rknpu2 --------- \033[0m"
+mv /packages/rknpu2/*.tar  /
+fi
 
-#------------------rktoolkit------------
-echo -e "\033[36m Install rktoolkit.................... \033[0m"
+echo -e "\033[47;36m ----- Install rktoolkit ----- \033[0m"
 \${APT_INSTALL} /packages/rktoolkit/*.deb
 
 # cho -e "\033[36m Install Chinese fonts.................... \033[0m"
@@ -237,16 +226,13 @@ echo -e "\033[36m Install rktoolkit.................... \033[0m"
 # HACK debian to fix bug
 \${APT_INSTALL} fontconfig --reinstall
 
-#------------------pulseaudio---------
-echo -e "\033[36m Install pulseaudio................. \033[0m"
+echo -e "\033[47;36m ----- Install pulseaudio ---- \033[0m"
 cp /etc/pulse/daemon.conf /
 cp /etc/pulse/default.pa /
 yes|\${APT_INSTALL} /packages/pulseaudio/*.deb
 mv /daemon.conf /default.pa /etc/pulse/
 
-apt install -fy --allow-downgrades /packages/install_packages/*.deb
-
-#------remove unused packages------------
+echo -e "\033[47;36m --- remove unused packages -- \033[0m"
 apt remove --purge -fy linux-firmware*
 
 # mark package to hold
@@ -255,12 +241,12 @@ apt list --installed | grep -v oldstable | cut -d/ -f1 | xargs apt-mark hold
 # mark rga package to unhold
 apt-mark unhold librga2 librga-dev librga2-dbgsym
 
-#---------------Custom Script--------------
+echo -e "\033[47;36m ------- Custom Script ------- \033[0m"
 systemctl mask systemd-networkd-wait-online.service
 systemctl mask NetworkManager-wait-online.service
 rm /lib/systemd/system/wpa_supplicant@.service
 
-#---------------Clean--------------
+echo -e "\033[47;36m  ---------- Clean ----------- \033[0m"
 if [ -e "/usr/lib/arm-linux-gnueabihf/dri" ] ;
 then
         cd /usr/lib/arm-linux-gnueabihf/dri/
@@ -277,7 +263,6 @@ then
 fi
 cd -
 
-#---------------Clean--------------
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/
 rm -rf /packages
--- mk-buster-lite.sh
+++ mk-buster-lite.sh
@@ -3,6 +3,37 @@
 # Directory contains the target rootfs
 TARGET_ROOTFS_DIR="binary"
 
+if [ ! $SOC ]; then
+	SOC="rk3568"
+fi
+
+install_packages() {
+    case $SOC in
+        rk3399|rk3399pro)
+		MALI=midgard-t86x-r18p0
+		ISP=rkisp
+		RGA=rga
+		;;
+        rk3328)
+		MALI=utgard-450
+		ISP=rkisp
+		RGA=rga
+		;;
+        rk356x|rk3566|rk3568)
+		MALI=bifrost-g52-g2p0
+		ISP=rkaiq_rk3568
+		RGA=rga
+		MIRROR=carp-rk356x
+		;;
+        rk3588|rk3588s)
+		ISP=rkaiq_rk3588
+		MALI=valhall-g610-g6p0
+		RGA=rga2
+		# MIRROR=carp-rk3588
+		;;
+    esac
+}
+
 case "${ARCH:-$1}" in
 	arm|arm32|armhf)
 		ARCH=armhf
@@ -30,11 +61,19 @@ finish() {
 trap finish ERR
 
 echo -e "\033[36m Extract image \033[0m"
+sudo rm -rf $TARGET_ROOTFS_DIR
 sudo tar -xpf linaro-buster-lite-alip-*.tar.gz
 
 # packages folder
 sudo mkdir -p $TARGET_ROOTFS_DIR/packages
 sudo cp -rf packages/$ARCH/* $TARGET_ROOTFS_DIR/packages
+
+#GPU/RGA/CAMERA packages folder
+install_packages
+sudo mkdir -p $TARGET_ROOTFS_DIR/packages/install_packages
+sudo cp -rpf packages/$ARCH/libmali/libmali-*$MALI*-x11*.deb $TARGET_ROOTFS_DIR/packages/install_packages
+sudo cp -rpf packages/$ARCH/camera_engine/camera_engine_$ISP*.deb $TARGET_ROOTFS_DIR/packages/install_packages
+sudo cp -rpf packages/$ARCH/$RGA/*.deb $TARGET_ROOTFS_DIR/packages/install_packages
 
 # overlay folder
 sudo cp -rf overlay/* $TARGET_ROOTFS_DIR/
@@ -65,10 +104,10 @@ sudo mount -o bind /dev $TARGET_ROOTFS_DIR/dev
 
 cat << EOF | sudo chroot $TARGET_ROOTFS_DIR
 
-echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian buster main" | sudo tee -a /etc/apt/sources.list
-echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian carp-rk356x main" | sudo tee -a /etc/apt/sources.list
-
-curl https://Embedfire.github.io/keyfile | sudo apt-key add -
+if [ $MIRROR ]; then
+	echo "deb [arch=arm64] https://cloud.embedfire.com/mirrors/ebf-debian $MIRROR main" | sudo tee -a /etc/apt/sources.list
+	curl https://Embedfire.github.io/keyfile | sudo apt-key add -
+fi
 
 apt-get update
 apt-get upgrade -y
@@ -78,7 +117,7 @@ chmod +x /etc/rc.local
 
 export APT_INSTALL="apt-get install -fy --allow-downgrades"
 
-#--------------- lubancat --------------
+echo -e "\033[47;36m ---------- LubanCat -------- \033[0m"
 \${APT_INSTALL} toilet
 
 passwd root <<IEOF
@@ -98,17 +137,15 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 # allow root login
 sed -i '/pam_securetty.so/s/^/# /g' /etc/pam.d/login
 
+apt install -fy --allow-downgrades /packages/install_packages/*.deb
+
 \${APT_INSTALL} ttf-wqy-zenhei xfonts-intl-chinese
 
-#---------------power management --------------
+echo -e "\033[47;36m ----- power management ----- \033[0m"
 \${APT_INSTALL} pm-utils triggerhappy
 cp /etc/Powermanager/triggerhappy.service  /lib/systemd/system/triggerhappy.service
 
-#---------------Rga--------------
-\${APT_INSTALL} /packages/rga/*.deb
-
-echo -e "\033[36m Setup Video.................... \033[0m"
-
+echo -e "\033[47;36m ------ Setup Video---------- \033[0m"
 \${APT_INSTALL} /packages/mpp/*
 \${APT_INSTALL} /packages/gst-rkmpp/*.deb
 # \${APT_INSTALL} /packages/gstreamer/*.deb
@@ -116,47 +153,38 @@ echo -e "\033[36m Setup Video.................... \033[0m"
 # \${APT_INSTALL} /packages/gst-plugins-good1.0/*.deb
 # \${APT_INSTALL} /packages/gst-plugins-ugly1.0/*.deb
 
-#---------Camera---------
-# echo -e "\033[36m Install camera.................... \033[0m"
+echo -e "\033[47;36m -----  Install Camera ----- - \033[0m"
 # \${APT_INSTALL} /packages/libv4l/*.deb
 
-#------------------libdrm------------
-echo -e "\033[36m Install libdrm.................... \033[0m"
+echo -e "\033[47;36m ------- Install libdrm ------ \033[0m"
 \${APT_INSTALL} /packages/libdrm/*.deb
 
-#------------------pcmanfm------------
-#echo -e "\033[36m Install pcmanfm.................... \033[0m"
+echo -e "\033[47;36m ----- Install pcmanfm ------- \033[0m"
 #\${APT_INSTALL} /packages/pcmanfm/*.deb
 
-#------------------blueman------------
-echo -e "\033[36m Install blueman.................... \033[0m"
-\${APT_INSTALL} blueman
-echo exit 101 > /usr/sbin/policy-rc.d
-chmod +x /usr/sbin/policy-rc.d
-\${APT_INSTALL} blueman
-rm -f /usr/sbin/policy-rc.d
+# echo -e "\033[47;36m ------ Install blueman ------ \033[0m"
+# \${APT_INSTALL} blueman
+# echo exit 101 > /usr/sbin/policy-rc.d
+# chmod +x /usr/sbin/policy-rc.d
+# \${APT_INSTALL} blueman
+# rm -f /usr/sbin/policy-rc.d
 
-#------------------rkwifibt------------
-echo -e "\033[36m Install rkwifibt.................... \033[0m"
-\${APT_INSTALL} /packages/rkwifibt/*.deb
-ln -sf /system/etc/firmware /vendor/etc/
+if [ -e "/usr/lib/aarch64-linux-gnu" ] ;
+then
+echo -e "\033[47;36m ------- move rknpu2 --------- \033[0m"
+mv /packages/rknpu2/*.tar  /
+fi
 
-#------------------rknpu2------------
-echo -e "\033[36m Install rknpu2.................... \033[0m"
-tar xvf /packages/rknpu2/*.tar -C /
-
-#------------------rktoolkit------------
-echo -e "\033[36m Install rktoolkit.................... \033[0m"
+echo -e "\033[47;36m ----- Install rktoolkit ----- \033[0m"
 \${APT_INSTALL} /packages/rktoolkit/*.deb
 
-#------------------pulseaudio---------
-echo -e "\033[36m Install pulseaudio................. \033[0m"
-cp /etc/pulse/daemon.conf /
-cp /etc/pulse/default.pa /
+echo -e "\033[47;36m ----- Install pulseaudio ---- \033[0m"
+mv /etc/pulse/daemon.conf /
+mv /etc/pulse/default.pa /
 yes|\${APT_INSTALL} /packages/pulseaudio/*.deb
 mv /daemon.conf /default.pa /etc/pulse/
 
-#------remove unused packages------------
+echo -e "\033[47;36m --- remove unused packages -- \033[0m"
 apt remove --purge -fy linux-firmware*
 
 # mark package to hold
@@ -165,12 +193,12 @@ apt list --installed | grep -v oldstable | cut -d/ -f1 | xargs apt-mark hold
 # mark rga package to unhold
 apt-mark unhold librga2 librga-dev librga2-dbgsym
 
-#---------------Custom Script--------------
+echo -e "\033[47;36m ------- Custom Script ------- \033[0m"
 systemctl mask systemd-networkd-wait-online.service
 systemctl mask NetworkManager-wait-online.service
 rm /lib/systemd/system/wpa_supplicant@.service
 
-#---------------Clean--------------
+echo -e "\033[47;36m  ---------- Clean ----------- \033[0m"
 if [ -e "/usr/lib/arm-linux-gnueabihf/dri" ] ;
 then
         cd /usr/lib/arm-linux-gnueabihf/dri/
@@ -187,7 +215,6 @@ then
 fi
 cd -
 
-#---------------Clean--------------
 rm -rf /var/lib/apt/lists/*
 rm -rf /var/cache/
 rm -rf /packages/
